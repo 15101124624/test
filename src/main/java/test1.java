ca80043ba1c7687fbd8cb82ba372b525bcd82a99
gao <gao@163.com>
2020-05-05 16:35:32 +0800

    aa

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -3,6 +3,10 @@ public class test1 {
     private String name;
     private String address;
 
+    public String getAddress() {
+        return address;
+    }
+
     public void setAddress(String address) {
         this.address = address;
     }
